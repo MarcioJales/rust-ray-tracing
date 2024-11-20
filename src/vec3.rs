15d70c928ef3429075088864ce54eecb34c55b42
MarcioJales <marcio.dojcosta@gmail.com>
2024-11-20 20:28:23 -0300

    [sec 3] derive `Clone` trait to use same values on `sum_sub` test

--- src/vec3.rs
+++ src/vec3.rs
@@ -1,6 +1,6 @@
-use std::ops::{Add, Index, IndexMut};
+use std::ops::{Add, Index, IndexMut, Sub};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Vec3 {
     e: [f64; 3]
 }
@@ -28,6 +28,22 @@ impl Add for Vec3 {
         res.e[0] = self.e[0] + rhs.e[0];
         res.e[1] = self.e[1] + rhs.e[1];
         res.e[2] = self.e[2] + rhs.e[2];
+
+        res
+    }
+
+    type Output = Vec3;
+}
+
+impl Sub for Vec3 {
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut res = Vec3 {
+            e: [0.0, 0.0, 0.0]
+        };
+
+        res.e[0] = self.e[0] - rhs.e[0];
+        res.e[1] = self.e[1] - rhs.e[1];
+        res.e[2] = self.e[2] - rhs.e[2];
 
         res
     }
@@ -65,7 +81,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sum() {
+    fn sum_sub() {
         let v1 = Vec3{
             e: [2.0, 6.0, 9.0],
         };
@@ -74,9 +90,11 @@ mod tests {
             e: [1.0, 1.0, 1.0],
         };
 
-        let v3 = v1 + v2;
+        let v3 = v1.clone() + v2.clone();
+        let v4 = v1 - v2;
 
-        assert_eq!(v3.e, [3.0, 7.0, 10.0])
+        assert_eq!(v3.e, [3.0, 7.0, 10.0]);
+        assert_eq!(v4.e, [1.0, 5.0, 8.0]);
     }
 
     #[test]
