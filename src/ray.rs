6c9d8a1551494eba256e601a7a1904274c5899a8
MarcioJales <marcio.dojcosta@gmail.com>
2024-12-11 21:41:18 -0300

    feat(section-4): create the methods to get elements and the ray formula

--- src/ray.rs
+++ src/ray.rs
@@ -4,3 +4,22 @@ pub struct Ray {
     orig: Vec3,
     dir: Vec3
 }
+
+impl Ray {
+    pub fn origin(self) -> Vec3 { 
+        self.orig
+    }
+
+    pub fn direction(self) -> Vec3 { 
+        self.dir
+    }
+
+    pub fn at(self, t: f64) -> Vec3 {
+        self.orig + t * self.dir
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
