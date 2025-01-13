6bc2a38036de28f61f6f3568013809910a788eaf
MarcioJales <marcio.dojcosta@gmail.com>
2025-01-12 21:20:30 -0300

    refactor(section-7): `render()` and `write_color()` of `Camera`

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod sphere;
 mod interval;
 mod camera;
 
+use camera::Camera;
 use hittable::{HitRecord, Hittable, HittableList};
 use interval::Interval;
 use sphere::Sphere;
@@ -34,46 +35,12 @@ fn main() {
         }
     ));
 
-    let mut f = File::create("image.ppm").unwrap();
+    let f = File::create("image.ppm").unwrap();
 
-    writeln!(f, "P3").unwrap();
-    writeln!(f, "{image_width} {image_height}").unwrap();
-    writeln!(f, "255").unwrap();
+    let mut cam: Camera = Default::default();
 
-    let (mut j, mut i) = (0, 0);
+    cam.aspect_ratio = 16.0/9.0;
+    cam.image_width = 400;
 
-    while j < image_height {
-        i = 0;
-        println!("\rScanlines remaining: {}", image_height - j);
-        while i < image_width {
-            let pixel_center = pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
-            let ray_direction = pixel_center - camera_center;
-            let ray = Ray{
-                orig: camera_center,
-                dir: ray_direction
-            };
-
-            let pixel_color = ray_color(ray, &world);
-
-            write_color(pixel_color, &mut f);
-
-            i += 1;
-        }
-        j += 1;
-    }
-
-    println!("\rDone.                 ");
-}
-
-fn write_color(pixel_color: Vec3, f: &mut File) {
-    let r = pixel_color.x();
-    let g = pixel_color.y();
-    let b = pixel_color.z();
-
-    // Translate the [0,1] component values to the byte range [0,255].
-    let rbyte = (255.999 * r) as i64;
-    let gbyte = (255.999 * g) as i64;
-    let bbyte = (255.999 * b) as i64;
-
-    writeln!(f, "{} {} {}", rbyte, gbyte, bbyte).unwrap();
+    cam.render(world, f);
 }
