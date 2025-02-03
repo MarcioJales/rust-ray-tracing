cb87c7951e6357445460f2676df986bb00f6d52e
MarcioJales <marcio.dojcosta@gmail.com>
2025-02-02 23:39:29 -0300

    feat(section-10):  implement the `Lambertian` material.
    Since attenuation and scattered ray references were being used as " outputs", I've changed the implementation to return them both as a tuple wrapped in a `Option`

--- src/material.rs
+++ src/material.rs
@@ -3,7 +3,24 @@ use crate::HitRecord;
 use crate::Vec3;
 
 pub trait Material {
-    fn scatter(&self, ray_in: &Ray, hit_record: &HitRecord,  attenuation: &Vec3, scattered: &Ray) -> bool {
-        return false;
+    fn scatter(&self, ray_in: &Ray, hit_record: &HitRecord) -> Option<(Ray, Vec3)>{
+        return None;
+    }
+}
+
+pub struct Lambertian {
+    albedo: Vec3
+}
+
+impl Material for Lambertian {
+    fn scatter(&self, ray_in: &Ray, hit_record: &HitRecord) -> Option<(Ray, Vec3)> {
+        let scatter_direction = hit_record.normal + Vec3::random_unit();
+        let scattered = Ray {
+            orig: hit_record.point,
+            dir: scatter_direction
+        };
+        let attenuation = self.albedo;
+
+        Some((scattered, attenuation))
     }
 }
